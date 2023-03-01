aeb523cca433e536be06ac68711eb5f2e009ff9b
Aneckon <aneckogri@gmail.com>
2023-03-01 16:20:05 +0200

    fix: googlemanager

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,18 +28,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         {/* <!-- Google Tag Manager --> */}
-        <Script>
-          {(function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-            var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s),
-              dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-          })(window, document, 'script', 'dataLayer', 'GTM-TSPTKGQ')}
-        </Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TSPTKGQ');`,
+          }}></script>
         {/* <!-- End Google Tag Manager --> */}
       </Head>
 
@@ -64,7 +60,7 @@ export default function Home() {
           src="https://www.googletagmanager.com/ns.html?id=GTM-TSPTKGQ"
           height="0"
           width="0"
-          style="display:none;visibility:hidden"></iframe>
+          style={{ display: 'none', visibility: 'hidden' }}></iframe>
       </noscript>
       {/* <!-- End Google Tag Manager (noscript) --> */}
     </>
